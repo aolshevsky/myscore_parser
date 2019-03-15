2d526302277da13381a22a87009b8be0b152fae5
Unknown <campapp.games@gmail.com>
2019-03-15 18:03:58 +0300

    Create templates for parsing match info

---
+++ templates.py
@@ -0,0 +1,9 @@
+periods_block = {'class': 'detailMS'}
+periods_headers = {'class': 'detailMS__headerText'}
+period_row = {'class': 'detailMS__incidentRow'}
+period_row_time = {'class': ['time-box', 'time-box-wide']}
+period_row_y_card = {'class': 'y-card'}
+period_row_substitution_name = {'class': ['substitution-in-name', 'substitution-out-name']}
+period_row_participant_name = {'class': 'participant-name'}
+period_row_soccer_ball = {'class': 'soccer-ball'}
+period_row_sub_incident_name = {'class': 'subincident-name'}
